de0ef64bca560a165dbfb4b818820a35ecf752f9
Mohamed Abdelhafiz <mohamed.abdelhafiz.dev@gmail.com>
2024-11-28 04:53:26 +0200

    updated UI and made customized prompt.

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 let addButton = document.querySelector(".add-task-btn");
 let tasksDiv = document.querySelector(".tasks");
+let promptDiv = document.querySelector(".promptt");
+let promptmsg = document.querySelector(".promptt .prompt-msg");
+let promptinput = document.querySelector(".promptt .prompt-input");
+let saveBtn = document.querySelector(".promptt .save");
+let cancelBtn = document.querySelector(".promptt .cancel");
 let tasks = [];
 if (
   localStorage.getItem("tasks") &&
@@ -15,15 +20,61 @@ if (
   tasksDiv.classList.add("no-tasks");
 }
 addButton.addEventListener("click", function (event) {
-  let taskTitle = window.prompt("Enter the task's title :");
+  promptDiv.classList.remove("hidden");
+  promptDiv.classList.add("prompt");
+  promptinput.focus();
+  document.querySelector("body").style.background = "#000000d9";
+});
+
+saveBtn.addEventListener("click", function (event) {
+  let taskTitle = promptinput.value;
+  if (taskTitle.trim() == "" && editIndex != null) {
+    deleteTask(editIndex);
+    editIndex = null;
+    promptDiv.classList.remove("prompt");
+    promptDiv.classList.add("hidden");
+    document.querySelector("body").style.background = "#000";
+  }
+  if (taskTitle.trim() == "" && editIndex == null) {
+    return;
+  }
+
   if (taskTitle !== null && taskTitle.trim().length > 0) {
+    if (editIndex != null && taskTitle == tasks[editIndex].title) {
+      promptinput.value = "";
+      promptDiv.classList.remove("prompt");
+      promptDiv.classList.add("hidden");
+      document.querySelector("body").style.background = "#0d1a2b";
+      editIndex = null;
+      return;
+    } else if (editIndex != null && taskTitle != tasks[editIndex].title) {
+      tasks[editIndex].title = taskTitle;
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      fillTasksOnThePage();
+      promptDiv.classList.remove("prompt");
+      promptDiv.classList.add("hidden");
+      document.querySelector("body").style.background = "#0d1a2b";
+      editIndex = null;
+      promptinput.value = "";
+      return;
+    }
     let task = createTask(taskTitle);
     tasks.push(task);
     localStorage.setItem("tasks", JSON.stringify(tasks));
     fillTasksOnThePage();
   }
+  promptDiv.classList.remove("prompt");
+  promptDiv.classList.add("hidden");
+  promptinput.value = "";
+  document.querySelector("body").style.background = "#0d1a2b";
 });
+cancelBtn.addEventListener("click", function () {
+  promptDiv.classList.add("hidden");
+  promptDiv.classList.remove("prompt");
+  promptinput.value = "";
 
+  document.querySelector("body").style.background = "#0d1a2b";
+});
 function createTask(taskTitle) {
   let task = {
     title: `${taskTitle}`,
@@ -87,13 +138,13 @@ function completeTask(index) {
   fillTasksOnThePage();
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
+let editIndex = null;
 function editTask(index) {
- let newTitle = prompt("Enter the new task's title", tasks[index].title);
-  if (newTitle !== null && newTitle.trim().length > 0) {
-    tasks[index].title = newTitle;
-  }
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-  fillTasksOnThePage();
+  promptDiv.classList.remove("hidden");
+  promptDiv.classList.add("prompt");
+  document.querySelector("body").style.background = "#000000d9";
+  editIndex = index;
+  promptinput.value = tasks[index].title;
 }
 function deleteTask(index) {
   tasks.splice(index, 1);
